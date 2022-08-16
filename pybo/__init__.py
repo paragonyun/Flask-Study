2b1f03e0463af9a03d06d1e156fdb04c5be6a659
paragonyun <paragonyun@naver.com>
2022-08-16 10:28:36 +0900

    db Making and flask shell

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -27,7 +27,8 @@ from flask import Flask
 
 ## ORM 적용 후
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy ## 얘는 Python Code로 SQL 할 수 있게 해주는 녀석임
+
 
 import config
 
@@ -41,6 +42,13 @@ def create_app() :
     ### ORM
     db.init_app(app)
     migrate.init_app(app, db) ## app에 db 등록!!
+    ## 여기가 새로 추가됨 (model import)##
+    from . import models 
+    ## 이후 (Terminal) flask db migrate
+    ## 이후 (Terminal) flask db upgrade => pybo.db 생성
+    ## 이후 SQLite 깔아서 db 까지 확인
+    ## 이후 (Terminal) flask shell 실행
+
 
     ## 블루 프린트 !!
     from .views import main_views
@@ -48,7 +56,12 @@ def create_app() :
 
     return app
 
-''' DB 초기화부터 다시 하기'''
+##(Terminal)flask db init으로 migrations 폴더 생성##
+## 굳이 얘네들에 대해 알 필요는 없음 ##
 
-
+'''
+꼭 알아야 하는 DB 관리 명령어
+flask db migrate : 모델을 새로 생성 및 변경할 때 사용
+flask db upgrade : 모델의 변경 내용을 실제 db에 적용할 때
+'''
 
---
+++ pybo/models.py
@@ -0,0 +1,42 @@
+'''
+우리가 만들 DB model columns
+
+id | subject | content | create_date
+'''
+
+from pybo import db ## __init__.py의 db를 가져옴
+
+## db.Model을 상속 받은 Question class로 SQL 작성
+class Question(db.Model) : ## class 이름으로 Table 이름이 생성됨
+    id = db.Column(db.Integer, primary_key = True) ## 칼럼 속성과 기본키인지 아닌지
+    ## 기본키가 되면 당연히 중복값 허용 안 한
+
+    subject = db.Column(db.String(200), nullable=False) ## Null값 허용 안 함
+    content = db.Column(db.Text(), nullable=False)
+    ## String과 Text의 차이 : String은 글자수 제한 O, Text는 X
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+
+## 답변 class도 생성
+
+class Answer (db.Model) :
+    id = db.Column(db.Integer, primary_key = True)
+    '''
+    답변과 질문을 연결하기 위한 속성 
+    연결 기준이 되는 key는 당연히 외래키가 됨 
+    ForeignKey('qwestion.id')는 qestion Table의 id Column을 가져오겠다는 말, 객체 아님!!!!
+    CASCADE : 질문이 삭제되면 답변도 함께 삭제된다는 말
+    '''
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    
+    '''
+    relationship('참조할 모델명', backref)
+    역참조(backref) : 질문에 달린 답변들을 볼 수 있게 하는 것 (한 질문엔 답변이 여러개 달릴 수 있다.)
+    '''
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+
+
